2545d6fa0095278ec8623abf08eaceef1e2f00eb
alghifarydaffa62 <88295192+alghifarydaffa62@users.noreply.github.com>
2023-06-21 21:30:14 +0700

    fix navbar

--- script.js
+++ script.js
@@ -25,11 +25,10 @@ function showSlides(n) {
     dots[slideIndex - 1].className += "active";
 }
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+function hamburg() {
+    hamburger = document.querySelector(".hamburger");
+    hamburger.onclick = function() {
+        navbar = document.querySelector(".nav-link");
+        navbar.classList.toggle("active");
     }
-  }
+}
